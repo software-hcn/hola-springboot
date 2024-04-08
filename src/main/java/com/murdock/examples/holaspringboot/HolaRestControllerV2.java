bef5438742b554eb27e9bfe0af13ee263f432340
caineng.huang <caineng.huang@weshareholdings.com>
2024-04-08 17:45:05 +0800

    feat[holaspringboot]: 外部配置

---
+++ src/main/java/com/murdock/examples/holaspringboot/HolaRestControllerV2.java
@@ -0,0 +1,45 @@
+package com.murdock.examples.holaspringboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @project hola-springboot
+ * @description 外部配置
+ * @author Asher.H
+ * @date 2024/4/8 17:41:39
+ * @version 1.0
+ */
+
+@RestController
+@RequestMapping("/api")
+@ConfigurationProperties(prefix = "helloapp")
+public class HolaRestControllerV2 {
+
+    private String saying;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/holaV2", produces = "text/plain")
+    public String hola() throws UnknownHostException {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost()
+                    .getHostAddress();
+        } catch (UnknownHostException e) {
+            hostname = "unknown";
+        }
+        return saying + " @ " + hostname;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+
+    public void setSaying(String saying) {
+        this.saying = saying;
+    }
+}
