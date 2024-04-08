02c024d3bf72df041ec019f1d57a814615115395
Asher.H <software.hcn@gmail.com>
2024-04-08 11:15:57 +0800

    First commit

---
+++ src/test/java/com/murdock/examples/holaspringboot/HolaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murdock.examples.holaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
