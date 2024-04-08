8acb659ff5dd5f9dadaedc2a65b99ebeda1e78db
caineng.huang <caineng.huang@weshareholdings.com>
2024-04-08 15:48:00 +0800

    feat[HTTP/REST端点]: 暴露HTTP/REST端点

---
+++ src/test/java/com/murdock/examples/holaspringboot/HolaRestControllerV1.java
@@ -0,0 +1,33 @@
+package com.murdock.examples.holaspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @project hola-springboot
+ * @description 
+ * @author Asher.H
+ * @date 2024/4/8 15:26:30
+ * @version 1.0
+ */
+
+@RestController
+@RequestMapping("/api")
+public class HolaRestControllerV1 {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/holaV1", produces = "text/plain")
+    public String hola() throws UnknownHostException {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost()
+                    .getHostAddress();
+        } catch (UnknownHostException e) {
+            hostname = "unknown";
+        }
+        return "Hola Spring Boot @ " + hostname;
+    }
+}
